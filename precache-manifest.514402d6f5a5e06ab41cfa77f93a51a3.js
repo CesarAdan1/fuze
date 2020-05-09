32ca42ec613b5a718a88cbe9270f8215bae52934
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-09 19:37:07 +0000

    Deploy fuze.8bit.codes to github.com/i-break-codes/fuze.git:gh-pages

---
+++ precache-manifest.514402d6f5a5e06ab41cfa77f93a51a3.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39ca34d5a11f26dfcfc6",
+    "url": "/css/app.a3ae6575.css"
+  },
+  {
+    "revision": "458f30d88988464ca54c",
+    "url": "/css/chunk-09d5625b.e49494a4.css"
+  },
+  {
+    "revision": "11b9da11670a58fb92dc74b8fa471d60",
+    "url": "/fonts/coves_light-webfont.11b9da11.woff2"
+  },
+  {
+    "revision": "45cb3e5e9736570390c95097b101d3b9",
+    "url": "/fonts/coves_light-webfont.45cb3e5e.woff"
+  },
+  {
+    "revision": "9e188e4faef9b1b22e7c6e292905686d",
+    "url": "/fonts/lombok-webfont.9e188e4f.woff"
+  },
+  {
+    "revision": "a441304a1420a9a59cbc8f13f5c1ed46",
+    "url": "/fonts/lombok-webfont.a441304a.woff2"
+  },
+  {
+    "revision": "f8e9fd0d9cf483fbdff1b3dd05fd1d75",
+    "url": "/img/fuze-banner.jpg"
+  },
+  {
+    "revision": "d6a117c6c392396f0f7378d277a66773",
+    "url": "/img/fuze-demo.gif"
+  },
+  {
+    "revision": "e115506db280636465a3b24f01f3e31a",
+    "url": "/img/try-it-out.jpg"
+  },
+  {
+    "revision": "eac8ec830aec4548fc326e2e6880494a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "39ca34d5a11f26dfcfc6",
+    "url": "/js/app.259e629f.js"
+  },
+  {
+    "revision": "458f30d88988464ca54c",
+    "url": "/js/chunk-09d5625b.a59dbe27.js"
+  },
+  {
+    "revision": "652e13ef2af975d180b0",
+    "url": "/js/chunk-vendors.33c25086.js"
+  },
+  {
+    "revision": "fdbf600bfe6d141c3429a03550d8348d",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
